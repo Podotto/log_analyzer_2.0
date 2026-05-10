1f89ac5e237f65174d7b66c053892a0141cd7fd5
Podotto <grethelhalphen@gmail.com>
2026-05-10 18:37:45 -0500

    Gráfica para ver eventos

--- src/visualizer.py
+++ src/visualizer.py
@@ -2,7 +2,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
+# Gráfica para ver intentos de logins
 def graph_logins():
+
     df = pd.read_csv(
         "data/logs2.txt", header=None, names=["Date", "User", "Event", "Status"]
     )
@@ -15,4 +17,20 @@ def graph_logins():
     plt.show()
 
 
-graph_logins()
+# Gráfica para ver tipos de eventos
+def graph_events():
+
+    df = pd.read_csv(
+        "data/logs2.txt", header=None, names=["Date", "User", "Event", "Status"]
+    )
+
+    event_count = df["Event"].value_counts()
+    ax = event_count.plot(kind="bar", color="blue")
+
+    for i, v in enumerate(event_count):
+        ax.text(i, v, str(v), fontweight="bold")
+
+    plt.show()
+
+
+graph_events()
