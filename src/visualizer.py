c1a1d10401f21130011aa0063fea86dc8cb8e2b9
Podotto <grethelhalphen@gmail.com>
2026-05-10 18:14:09 -0500

    Se crea la función

--- src/visualizer.py
+++ src/visualizer.py
@@ -1,13 +1,18 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv(
-    "data/logs2.txt", header=None, names=["Date", "User", "Event", "Status"]
-)
 
-status_count = df["Status"].value_counts()
-status_count.plot(kind="barh", color="violet")
+def graph_logins():
+    df = pd.read_csv(
+        "data/logs2.txt", header=None, names=["Date", "User", "Event", "Status"]
+    )
 
-plt.title("Logins Status")
-plt.xlabel("Attempts")
-plt.show()
+    status_count = df["Status"].value_counts()
+    status_count.plot(kind="barh", color="violet")
+
+    plt.title("Logins Status")
+    plt.xlabel("Attempts")
+    plt.show()
+
+
+graph_logins()
